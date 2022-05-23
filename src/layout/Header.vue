83588e33d0bb5e2496f30cb7f3743f80664d3da2
Prajval <prajvalhl@gmail.com>
2022-05-23 17:49:27 +0530

    added header component

---
+++ src/layout/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="navbar navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/logo-white.png" class="brand-img" />
+    </a>
+    <span class="navbar-text text-white">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  props: ["title"],
+};
+</script>
+
+<style>
+.brand-img {
+  width: 30px;
+}
+</style>
